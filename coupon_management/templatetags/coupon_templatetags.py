304ca8fe187a3ff22201bd6a81a8c88fda72908b
ramshidmobiz <ramshid@mobiz.com>
2025-09-10 12:12:46 +0530

    whatsapp inte

--- coupon_management/templatetags/coupon_templatetags.py
+++ coupon_management/templatetags/coupon_templatetags.py
@@ -22,20 +22,28 @@ def available_free_coupons(coupon_pk):
 @register.simple_tag
 def get_coupon_designation(pk):
     context = {}
+    customer_pk = ""
+    customer_name = ""
+    print(pk)
+    print("pk")
     instance = NewCoupon.objects.get(pk=pk)
     coupon_status = CouponStock.objects.get(couponbook=instance).coupon_stock
     # print(CouponStock.objects.get(couponbook=instance).couponbook.pk)
+    print(coupon_status)
     
     if coupon_status == "customer":
         customer_instance = CustomerCouponItems.objects.filter(coupon=instance).first()
         # print(customer_instance.coupon)
         # if customer_instance.customer_coupon == None:
         #     customer_instance.delete()
-        customer_instance = customer_instance.customer_coupon.customer
+        if customer_instance:
+            customer_instance = customer_instance.customer_coupon.customer
+            customer_pk = customer_instance.pk,
+            customer_name = f"{customer_instance.custom_id} - {customer_instance.customer_name}",
         
         context = {
-            "pk": customer_instance.pk,
-            "name": f"{customer_instance.custom_id} - {customer_instance.customer_name}",
+            "pk": customer_pk,
+            "name": customer_name,
         }
     
     elif coupon_status == "van":
@@ -63,6 +71,7 @@ def get_coupon_designation(pk):
             "pk": "",
             "name": f"Used",
         }
+    print(context)
     return context
 
 
