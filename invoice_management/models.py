423b4d5df934f7182436b692b1d54f132860b3be
Josnacc <josnacc96@gmail.com>
2025-11-28 11:41:09 +0530

    invoice

--- invoice_management/models.py
+++ invoice_management/models.py
@@ -1,8 +1,6 @@
 import random
 import uuid
-import datetime
 from django.db import models
-from django.db import transaction
 
 from accounts.models import CustomUser, Customers
 from master.models import CategoryMaster, RouteMaster
@@ -23,7 +21,6 @@ class Invoice(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     reference_no = models.CharField(max_length=200)
     invoice_no = models.CharField(max_length=200)
-    invoice_number = models.IntegerField(null=True,blank=True)
     invoice_type = models.CharField(max_length=200, choices=INVOICE_TYPES,default='cash_invoice')
     invoice_status = models.CharField(max_length=200, choices=INVOICE_STATUS,default='non_paid')
     created_date = models.DateTimeField()
@@ -45,37 +42,21 @@ class Invoice(models.Model):
         return f'{self.id}'
     
     def save(self, *args, **kwargs):
-        if not self.created_date:
-            self.created_date = datetime.datetime.now()
-
         if not self.invoice_no:
-            year = self.created_date.strftime("%y")
-            prefix = f"IN-{year}/"
+            date = self.created_date.date()
+            date_part = date.strftime('%Y%m%d')
+            prefix = "WTR"
 
-            with transaction.atomic():
-                now = datetime.datetime.now()
-                
-                last_invoice = (
-                    Invoice.objects
-                    .filter(created_date__year=now.year)
-                    .select_for_update()
-                    .order_by("-invoice_number")
-                    .first()
-                )
+            # Count existing invoices with the same date
+            invoice_count = Invoice.objects.filter(
+                created_date__date=date,
+                invoice_no__startswith=f"{prefix}-{date_part}",
+                is_deleted=False,
+            ).count()
 
-                if last_invoice:
-                    last_num = last_invoice.invoice_number or 0 # <-- FIX
-                    print("last_num",last_num)
-                    new_num = last_num + 1
-                else:
-                    new_num = 1
+            new_number = invoice_count + 1
 
-                self.invoice_number = new_num
-                self.invoice_no = f"{prefix}{new_num}"
-
-      
-        if self.amout_total != self.amout_recieved:
-            self.invoice_type = "credit_invoice"
+            self.invoice_no = f"{prefix}-{date_part}-{new_number:04d}"  # Always 4-digit padded
 
         super().save(*args, **kwargs)
     
